52f7224e64a3e1fa8472f86c285da3a9121294c1
NeilSMyers <mmyersneil@gmail.com>
2020-11-15 13:49:38 -0700

    infinite scroll babyyyy

--- Home.js
+++ Home.js
@@ -2,24 +2,36 @@ import React from "react"
 import { StatusBar } from "expo-status-bar"
 
 import { View, StyleSheet, FlatList } from "react-native"
-import { useQuery } from "react-query"
+import { useInfiniteQuery } from "react-query"
 
 import PokemonCard from "./PokemonCard"
 
+const fetchPokemon = (_, cursor) => {
+  return fetch(cursor || "https://pokeapi.co/api/v2/pokemon/")
+    .then((res) => res.json())
+    .catch(console.log)
+}
+
 const Home = () => {
-  const { data } = useQuery("pokemon", () =>
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
-      .then((res) => res.json())
-      .then((data) => data.results)
+  const { data, canFetchMore, fetchMore } = useInfiniteQuery(
+    "pokemon",
+    fetchPokemon,
+    {
+      getFetchMore: (lastPage) => lastPage.next,
+    }
   )
 
   return (
     <View>
       <StatusBar style="light-content" />
       <FlatList
-        data={data}
+        data={data
+          ?.map((page) => page.results)
+          .reduce((all, page) => [...all, ...page])}
         renderItem={({ item }) => <PokemonCard {...item} />}
         keyExtractor={(item) => item.name}
+        onEndReachedThreshold={5}
+        onEndReached={() => canFetchMore && fetchMore()}
       />
     </View>
   )
